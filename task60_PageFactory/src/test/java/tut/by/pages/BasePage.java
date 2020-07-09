926f8f8a1605f95d9a7756e7a2f439fc8485e55d
IlyaBaek <ilyabaek@coherentsolutions.com>
2020-07-09 13:03:27 +0300

    final commit PageFactory(refactoring + Singleton)

--- task60_PageFactory/src/test/java/tut/by/pages/BasePage.java
+++ task60_PageFactory/src/test/java/tut/by/pages/BasePage.java
@@ -1,4 +1,6 @@
 package tut.by.pages;
+
+import WebDriverSingleton.WebDriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -9,19 +11,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BasePage {
     private static final int TIMEOUT = 5;
     private static final int POLLING = 100;
-    protected WebDriver driver;
     private WebDriverWait wait;
 
-    public BasePage(WebDriver driver){
-        this.driver = driver;
+    public BasePage() {
+        WebDriver driver = WebDriverSingleton.getInstance().getDriver();
         wait = new WebDriverWait(driver, TIMEOUT, POLLING);
-        PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT),this);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
     }
 
-    protected void clickOn(WebElement locator){
-       wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    protected void clickOn(WebElement locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
-    protected void typeIn(WebElement locator,String string){
+
+    protected void typeIn(WebElement locator, String string) {
         wait.until(ExpectedConditions.elementToBeClickable(locator)).sendKeys(string);
     }
 }
+
+/*
+    protected WebElement getElement(By locartor) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locartor));
+    }
+
+    protected boolean isEnabled(By locator) {
+        return getElement(locator).isEnabled();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        return getElement(locator).isDisplayed();
+    }
+
+    protected String getText(By locator) {
+        return getElement(locator).getText();
+    }
+*/
--- task60_PageFactory/src/test/java/tut/by/pages/LoginPage.java
+++ task60_PageFactory/src/test/java/tut/by/pages/LoginPage.java
@@ -1,12 +1,9 @@
 package tut.by.pages;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import static org.testng.Assert.assertTrue;
-
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
     @FindBy(name = "login")
     private WebElement LOGIN_INPUT;
     @FindBy(name = "password")
@@ -14,14 +11,14 @@ public class LoginPage extends BasePage{
     @FindBy(css = "div.b-hold>input[type='submit']")
     private WebElement LOGIN_BUTTON;
 
-    public LoginPage(WebDriver driver){
-        super(driver);
-        assertTrue(LOGIN_BUTTON.isDisplayed());
+    public LoginPage() {
+        LOGIN_BUTTON.isDisplayed();
     }
 
-    public void logIn(String login, String password){
-        typeIn(LOGIN_INPUT,login);
-        typeIn(PASSWORD_INPUT,password);
+    public LoggedHomePage logIn(String login, String password) {
+        typeIn(LOGIN_INPUT, login);
+        typeIn(PASSWORD_INPUT, password);
         clickOn(LOGIN_BUTTON);
+        return new LoggedHomePage();
     }
 }
